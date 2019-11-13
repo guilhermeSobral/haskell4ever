05f754cc1e626146d361292d9d45071098fd03ff
Ubuntu <ubuntu@ip-172-31-27-56.ec2.internal>
2019-11-13 05:19:44 +0000

    login

--- src/Handler/Entrar.hs
+++ src/Handler/Entrar.hs
@@ -23,4 +23,32 @@ getEntrarR = do
         $(whamletFile "templates/login/login.hamlet")
         addStylesheetRemote "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         toWidgetHead $(luciusFile "templates/login/login.lucius")
-        toWidgetHead $(juliusFile "templates/login/login.julius") 
+        toWidgetHead $(juliusFile "templates/login/login.julius")
+        
+postEntrarR :: Handler Html
+postEntrarR = do 
+    ((result,_),_) <- runFormPost formLogin
+    case result of
+        FormSuccess ("root@root.com", "root") -> do
+            setSession "_NOME" "Root"
+            redirect AdminR
+        FormSuccess (email,senha) -> do
+            usuario <- runDB $ getBy (UniqueEmailAdm email)
+            case usuario of
+                Nothing -> do
+                    setMessage [shamlet|
+                        <div>
+                            E-mail nao encontrado!
+                    |]
+                    redirect LoginR
+                Just (Entity _ usr) -> do
+                    if (usuarioSenha usr == senha) then do
+                        setSession "_NOME" (usuarioNome usr)
+                        redirect HomeR
+                    else do
+                        setMessage [shamlet|
+                            <div>
+                                Senha invalida!
+                    |]
+                    redirect LoginR
+        _ -> redirect HomeR        
