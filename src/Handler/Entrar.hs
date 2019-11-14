bb746f55f9154c591b9be1fe295412a2fcc81161
Ubuntu <ubuntu@ip-172-31-27-56.ec2.internal>
2019-11-14 02:27:29 +0000

    login

--- src/Handler/Entrar.hs
+++ src/Handler/Entrar.hs
@@ -31,7 +31,7 @@ postEntrarR = do
         <$> ireq emailField "email"
         <*> ireq passwordField "senha"
     case result of
-        FormSuccess ("root@root.com", "root") -> do
+        FormSuccess (Login "root@root.com" "root") -> do
             setSession "_NOME" "Root"
             redirect AdminR
         _ -> redirect HomeR        
