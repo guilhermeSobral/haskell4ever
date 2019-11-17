fd8f3cc680813e04f53c90c91523aaebe11f06b3
Ubuntu <ubuntu@ip-172-31-27-56.ec2.internal>
2019-11-17 07:59:41 +0000

    deixando organizado3

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -23,7 +23,7 @@ mkYesodData "App" $(parseRoutesFile "config/routes")
 
 instance Yesod App where
     makeLogger = return . appLogger
-    authRoute _ = (Just PrincipalR)
+    authRoute _ = (Just EntrarR)
     isAuthorized PrincipalR _ = return Authorized
     isAuthorized (StaticR _) _ = return Authorized
     isAuthorized EntrarR _ = return Authorized
